e162e026555e948553e5abc40b7f14d10d2d7187
amirrezaask <raskarpour@gmail.com>
2020-10-10 23:49:41 +0330

    fix center

--- lua/start/init.lua
+++ lua/start/init.lua
@@ -3,7 +3,8 @@ local api = vim.api
 
 local M = {}
 
-local function count_lines(ascii)
+
+local function string_to_lines_table(ascii)
    local lines = {}
    local line_start = 1
    for i=1,#ascii do
@@ -34,20 +35,45 @@ M['default_ascii_2'] = [[
 /_/ |_/\___/\____/|___/_/_/ /_/ /_/ 
 
 ]]
-local function start_point(width, height, lines)
-   local h = math.ceil(height - #lines / 2)
-   local w = math.ceil(width - #lines[1] / 2)
+local function start_point(width, height, ascii_col, ascii_lines)
+   local h = math.ceil(height /2 ) - (ascii_lines / 2)
+   local w = math.ceil(width /2 ) - ( ascii_col / 2)
+   print(string.format('start width %s', w))
+   print(string.format('start height', h))
    return w,h
 end
 
+local function str_repeat(str, number)
+   s = ""
+   for i=1,number do
+      s = s .. str
+   end
+   return s 
+end
 
 M['set_background_ascii'] = function(ascii)
-   local height = vim.api.nvim_get_option('columns')
-   local width = vim.api.nvim_get_option('lines')
-   local lines = count_lines(ascii) 
+   local height = vim.api.nvim_get_option('lines')
+   local width = vim.api.nvim_get_option('columns')
+   local lines = string_to_lines_table(ascii) 
+   local ascii_col = #lines[1]
+   local ascii_lines = #lines
+   print(string.format("ascii col %s", ascii_col))
+   print(string.format("ascii row %s", ascii_lines))
+   local start_col, start_row = start_point(width, height, ascii_col, ascii_lines)
+   
    if vim.fn.argc() < 1 then
       local buf = vim.api.nvim_create_buf(true, true) 
-      vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+      for i, _ in ipairs(lines) do
+         lines[i] = (str_repeat(' ', start_col)) .. (lines[i])
+      end
+      local new_lines = {}
+      for i=1,start_row do
+         table.insert(new_lines, '')
+      end
+      for _, l in ipairs(lines) do
+         table.insert(new_lines, l)
+      end
+      vim.api.nvim_buf_set_lines(buf, 0, -1, false, new_lines)
       vim.api.nvim_buf_set_option(buf, 'modifiable', false)
       vim.api.nvim_buf_set_option(buf, 'buflisted', false)
       local win = api.nvim_get_current_win()
